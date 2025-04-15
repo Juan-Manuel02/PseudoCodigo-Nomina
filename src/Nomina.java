cc29e5d2d477ca899f5a345f802b8041737fa8bb
Juan-Manuel02 <juanmanuelav2002@gmail.com>
2025-04-15 20:41:41 +0200

    Agrega clase Nomina para cálculo de pagos

--- src/Nomina.java
+++ src/Nomina.java
@@ -2,18 +2,22 @@ public class Nomina {
     private double nHoras;
     private double tarifaHoras;
     private double salarioBruto;
+    private Personal p;
     private final double D_DEUDA_SOCIAL = 3.46;
     private final double D_NO_COMUN = 6.15;
     private final double D_SEG_MEDICO = 0.95;
     private final double D_FONDO_P = 4.44;
+    private final double D_SEG_DES = 3.05;
     private final double D_PENSION_C = 3.05;
     private final double D_CONTR_JUB_ANT = 1.02;
 
     // Constructor
-    public Nomina(double nHoras, double tarifaHoras, double salarioBruto) {
+    public Nomina(double nHoras, double tarifaHoras, Personal p) {
         this.nHoras = nHoras;
         this.tarifaHoras = tarifaHoras;
         this.salarioBruto = salarioBruto;
+        this.p = p;
+        calcularSalarioBruto();
     }
 
     // Getter y Setter
@@ -31,5 +35,65 @@ public class Nomina {
 
     public void setTarifaHoras(double tarifaHoras) {
         this.tarifaHoras = tarifaHoras;
+    }
+    // Calculamos la prima familiar
+    public double calcularFrimaFamiliar (){
+        double primaFamiliar = 0;
+        if (this.p.getnHijos() == 1){
+            primaFamiliar = 20;
+        }else if (this.p.getnHijos () == 2){
+            primaFamiliar = 50;
+        }else if (this.p.getnHijos () > 2){
+            primaFamiliar = 75 + (this.p.getnHijos() - 2) * 20;
+        }
+        return primaFamiliar;
+    }
+    // Calculamos el salario bruto
+    public void calcularSalarioBruto(){
+        double salarioBruto = 0, horasNormales, horasExtra50, horasExtra60;
+        if (this.nHoras < 169){
+            salarioBruto = this.nHoras * this.tarifaHoras;
+        }else if (this.nHoras <= 180){
+            horasNormales = 169;
+            horasExtra50 = this.nHoras - horasNormales;
+            salarioBruto = horasNormales * tarifaHoras + horasExtra50 * 1.5;
+        }else {
+            horasNormales = 169;
+            horasExtra50 = 180 - horasNormales;
+            horasExtra60 = this.nHoras - 180;
+            salarioBruto = horasNormales * tarifaHoras + horasExtra50 * 1.5;
+        }
+        this.salarioBruto = salarioBruto;
+    }
+    // Calcular las deducciones
+    public double calculoDeduccion (double porcentaje){
+        return (porcentaje/100) * this.salarioBruto;
+    }
+    //Mostramos la nomina
+    public void mostrarNomina (){
+        double dedSocial, dedConComun, dedMedico, dedFondoP, dedPensionC, dedSegDes, salarioNeto, deduccionTotal, dedContrJubAnt;
+        System.out.println(p.toString());
+        System.out.println("Salario Bruto -> " + this.salarioBruto);
+        System.out.println("\nCalculo de Deducciones");
+        dedSocial = this.calculoDeduccion(this.D_DEUDA_SOCIAL);
+        System.out.println("Deducción Social ->" + dedSocial);
+        dedConComun = this.calculoDeduccion(this.D_NO_COMUN);
+        System.out.println("Deducción de Contingencias no Comunes -> " + dedConComun);
+        dedMedico = this.calculoDeduccion(this.D_SEG_MEDICO);
+        System.out.println("Deducción de Seguro Medico -> " + dedMedico);
+        dedFondoP = this.calculoDeduccion(this.D_FONDO_P);
+        System.out.println("Deducción de Fondo de Pensiones -> " + dedFondoP);
+        dedSegDes = this.calculoDeduccion(this.D_SEG_DES);
+        System.out.println("Deducción Seguro de Desempleo -> " + dedSegDes);
+        dedPensionC = this.calculoDeduccion(this.D_PENSION_C);
+        System.out.println("Deducción Pension Complementaria -> " + dedPensionC);
+        dedContrJubAnt = this.calculoDeduccion(this.D_CONTR_JUB_ANT);
+        System.out.println("Deducción Jubilación Anticipada -> " + dedContrJubAnt);
+
+        deduccionTotal = this.salarioBruto - dedPensionC - dedFondoP - dedMedico - dedContrJubAnt - dedConComun - dedSegDes - dedSegDes;
+        System.out.println("\nTotal de deducciones -> " + dedSocial);
+        salarioNeto = this.salarioBruto - deduccionTotal;
+        System.out.println("Prima familiar -> " + calcularFrimaFamiliar());
+        System.out.println("Prima Neto -> " + salarioNeto + calcularFrimaFamiliar());
     }
 }
