ffd35b3a8e65464a68995a4ef85f2e2e105bfb0a
Juan-Manuel02 <juanmanuelav2002@gmail.com>
2025-04-15 18:41:58 +0200

    Clase Main con punto de entrada del programa

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Personal emp1 = new Personal ("Pedro", "Lopez", (byte)4, "003");
+        System.out.println(emp1);
+        Personal emp2 = new Personal ("Nuria", "Cano", (byte)1, "001");
+        System.out.println(emp2);
+
+
+
+
+
+
+
+    }
+}
