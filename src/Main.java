36f5d75fbd5686895184b9c19af67d9508d565e3
Juan-Manuel02 <juanmanuelav2002@gmail.com>
2025-04-15 20:41:53 +0200

    Corrección en el main

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ public class Main {
     public static void main(String[] args) {
         Personal emp1 = new Personal ("Pedro", "Lopez", (byte)4, "003");
         System.out.println(emp1);
+
         Personal emp2 = new Personal ("Nuria", "Cano", (byte)1, "001");
         System.out.println(emp2);
 
+        Nomina n = new Nomina(190, 9.76,emp1);
+        n.mostrarNomina();
 
-
-
-
-
-
+        Nomina n2 = new Nomina(180, 8.78,emp2);
+        n2.mostrarNomina();
     }
 }
